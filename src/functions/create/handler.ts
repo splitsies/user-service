a0ce3fffee562404f2a7c9eaca3186703e4abb50
Kevin Chen <kevchen21@gmail.com>
2023-09-30 19:29:14 -0700

    fix handler

--- src/functions/create/handler.ts
+++ src/functions/create/handler.ts
@@ -1,26 +1,26 @@
 import schema from "./schema";
 import { middyfy } from "../../libs/lambda";
 import { container } from "../../di/inversify.config";
-import { HttpStatusCode, DataResponse, InvalidArgumentsError, CreateUserRequest } from "@splitsies/shared-models";
+import { HttpStatusCode, DataResponse, InvalidArgumentsError, CreateUserRequest, IUserCredential } from "@splitsies/shared-models";
 import { SplitsiesFunctionHandlerFactory, ILogger } from "@splitsies/utils";
 import { IUserService } from "src/services/user-service/user-service-interface";
-import { IUserMapper } from "src/mappers/user-mapper/user-mapper-interface";
-import { IUserDto } from "src/models/user-dto/user-dto-interface";
 
 const logger = container.get<ILogger>(ILogger);
 const userService = container.get<IUserService>(IUserService);
-const mapper = container.get<IUserMapper>(IUserMapper);
 
 export const main = middyfy(
-    SplitsiesFunctionHandlerFactory.create<typeof schema, IUserDto>(logger, async (event) => {
+    SplitsiesFunctionHandlerFactory.create<typeof schema, IUserCredential>(logger, async (event) => {
         if (!event.body.user) return new DataResponse(HttpStatusCode.BAD_REQUEST, "'user' was missing");
 
         try {
             const result = await userService.createUser(event.body.user as CreateUserRequest);
-            return new DataResponse(HttpStatusCode.CREATED, mapper.toDtoModel(result)).toJson();
+            return new DataResponse(HttpStatusCode.CREATED, result).toJson();
         } catch (e) {
+
+            logger.error(e);
+
             if (e instanceof InvalidArgumentsError) {
-                return new DataResponse(HttpStatusCode.BAD_REQUEST, e.message);
+                return new DataResponse(HttpStatusCode.BAD_REQUEST, e.message).toJson();
             }
 
             throw e;
