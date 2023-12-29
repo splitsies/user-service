f18ebdfa9963b9987a6759892195e2bd8b59faca
Kevin Chen <kevchen21@gmail.com>
2023-12-29 11:31:23 -0500

    log error on request failure

--- src/api/expense-api-client/expense-api-client.ts
+++ src/api/expense-api-client/expense-api-client.ts
@@ -1,13 +1,14 @@
 import { IExpenseUserDetails } from "@splitsies/shared-models";
 import { IExpenseApiClient } from "./expense-api-client-interface";
 import { inject, injectable } from "inversify";
-import { SplitsiesApiClientBase } from "@splitsies/utils";
+import { ILogger, SplitsiesApiClientBase } from "@splitsies/utils";
 import { IApiConfiguration } from "src/models/configuration/api/api-configuration-interface";
 import { IAuthHeaderProvider } from "src/providers/auth-header-provider/auth-header-provider-interface";
 
 @injectable()
 export class ExpenseApiClient extends SplitsiesApiClientBase implements IExpenseApiClient {
     constructor(
+        @inject(ILogger) private readonly _logger: ILogger,
         @inject(IApiConfiguration) private readonly _apiConfiguration: IApiConfiguration,
 
         @inject(IAuthHeaderProvider) private readonly _authHeaderProvider,
@@ -16,7 +17,11 @@ export class ExpenseApiClient extends SplitsiesApiClientBase implements IExpense
     }
 
     async mergeGuestUsers(deletedGuestId: string, registeredUser: IExpenseUserDetails): Promise<void> {
-        const url = `${this._apiConfiguration.expenseApiUrl}guests/${encodeURIComponent(deletedGuestId)}`;
-        await this.putJson(url, { registeredUser }, this._authHeaderProvider.provide());
+        try {
+            const url = `${this._apiConfiguration.expenseApiUrl}guests/${encodeURIComponent(deletedGuestId)}`;
+            await this.putJson(url, { registeredUser }, this._authHeaderProvider.provide());
+        } catch (e) {
+            this._logger.error(e);
+        }
     }
 }
