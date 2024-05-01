66c6d7d7a36245321fad7304c090a820ad8193fe
Kevin Chen <kevchen21@gmail.com>
2024-05-01 14:50:59 -0700

    use quotes

--- utils/local-startup/startup.sh
+++ utils/local-startup/startup.sh
@@ -1,7 +1,7 @@
 FIREBASE_USER_EMULATOR_HOST=http://127.0.0.1:9099
 sls offline start \
     --host 0.0.0.0 \
-    --param='QUEUE_STAGE_NAME=local' \
+    --param="QUEUE_STAGE_NAME=local" \
     --param='DB_ACCESS_KEY_ID=null' \
     --param='DB_SECRET_ACCESS_KEY=null' \
     --param='DB_REGION=us-west-2' \
