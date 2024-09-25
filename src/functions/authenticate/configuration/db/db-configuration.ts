e5e108ed00172b2b143c5d30d86bda999bc9bf49
Kevin Chen <kevchen21@gmail.com>
2024-09-25 13:51:35 -0700

    updates

--- src/functions/authenticate/configuration/db/db-configuration.ts
+++ src/functions/authenticate/configuration/db/db-configuration.ts
@@ -1,21 +1,15 @@
 import { assert } from "console";
 
 export class DbConfiguration {
-    readonly dbAccessKeyId: string;
-    readonly dbSecretAccessKey: string;
     readonly dbRegion: string;
     readonly tableName: string;
     readonly endpoint: string;
 
     constructor() {
-        assert(!!process.env.dbAccessKeyId, "db access key was undefined");
-        assert(!!process.env.dbSecretAccessKey, "db secret access key was undefined");
         assert(!!process.env.dbRegion, "db region was undefined");
         assert(!!process.env.dbTableName, "db table name was undefined");
         assert(!!process.env.dbEndpoint, "db endpoint was undefined");
 
-        this.dbAccessKeyId = process.env.dbAccessKeyId;
-        this.dbSecretAccessKey = process.env.dbSecretAccessKey;
         this.dbRegion = process.env.dbRegion;
         this.tableName = process.env.dbTableName;
         this.endpoint = process.env.dbEndpoint;
